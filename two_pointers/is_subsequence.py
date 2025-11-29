648afb56b18cdf87a7170c4753d448974321ed8b
felipe limeira <limeira.felipe94@gmail.com>
2025-11-29 17:02:36 -0300

    added is_subsequence

---
+++ two_pointers/is_subsequence.py
@@ -0,0 +1,38 @@
+"""
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+A subsequence of a string is a new string that is formed from the original string by deleting
+some (can be none) of the characters without disturbing the relative positions of the remaining
+characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+"""
+
+
+def is_subsequence(s: str, t: str) -> bool:
+    if not s:
+        return True
+    i = 0
+    for char in t:
+        if char == s[i]:
+            i += 1
+        if i == len(s):
+            return True
+    return False
+
+
+def test_1():
+    assert is_subsequence("abc", "ahbgdc") is True
+
+
+def test_2():
+    assert is_subsequence("axc", "ahbgdc") is False
+
+
+def test_3():
+    assert is_subsequence("acb", "ahbgdc") is False
